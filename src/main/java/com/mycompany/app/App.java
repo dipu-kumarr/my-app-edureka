a534cc59b175bbee11776193cc8bb5760f3f8d37
Dipu kumar <105379708+dipu-kumarr@users.noreply.github.com>
2024-09-21 17:01:26 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from AWS & git & Dipu he is good !" );
+        System.out.println( "Hello World from AWS & git & Dipu he is good sdsds !" );
     }
 } 
